70355a79dcf4cfb048aedec77f2782d19e0dbb06
abhinandan <abhinandan@factacy.ai>
2024-03-08 13:07:00 +0530

    new changes in router and constants

--- router/cdb.router.js
+++ router/cdb.router.js
@@ -1,11 +1,19 @@
 
 
 import { Router } from "express";
-import {industrial_portfolio} from "../controllers/cdb.controller.js";
+import {deal30, deal60, deal90, industrial_portfolio, main_sector} from "../controllers/cdb.controller.js";
 
 const router = Router();
 
 router.route('/industrial_portfolio').get(industrial_portfolio);
 
+router.route('/deal').get(deal);
 
+router.route ('/deal30').get(deal30);
+
+router.route('/deal60').get(deal60);
+
+router.route('/deal90').get(deal90);
+
+router.route('/main_sector').get(main_sector);
 export default router;
