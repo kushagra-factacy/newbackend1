6d08f8bf2c13e53aea989f7593ac3d400a7cc44f
kushagra <kushagra>
2024-03-08 08:24:09 +0000

    Revert "Merge branch 'main' of https://github.com/kushagra-factacy/new_backend into main"
    This reverts commit a3985a827064a674a189e3bce3e7f56d3e9bee50, reversing
    changes made to 57a7af03b08055e6961255240f13fa4dc0645805.

--- router/cdb.router.js
+++ router/cdb.router.js
@@ -1,19 +1,12 @@
 
 
 import { Router } from "express";
-import {deal,deal30, deal60, deal90, industrial_portfolio, main_sector} from "../controllers/cdb.controller.js";
+import {industrial_portfolio} from "../controllers/cdb.controller.js";
 
 const router = Router();
 
 router.route('/industrial_portfolio').get(industrial_portfolio);
 
-router.route('/deal').get(deal);
 
-router.route ('/deal30').get(deal30);
-
-router.route('/deal60').get(deal60);
-
-router.route('/deal90').get(deal90);
-
-router.route('/main_sector').get(main_sector);
+// router.route('/search').get(deal);
 export default router;
