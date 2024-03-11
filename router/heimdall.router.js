de5d5832b984ad6c7aa22b5c9af9e31d9c3b6361
abhinandan <abhinandan@factacy.ai>
2024-03-11 19:04:43 +0530

    seed info details , investor

--- router/heimdall.router.js
+++ router/heimdall.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {comp, deal, deal30, deal60, deal90 ,trending, patents,seed_information,investor_alt,seed_info_detail} from "../controllers/heimdall.controller.js";
+import {comp, deal, deal30, deal60, deal90 ,trending, patents,seed_information,investor_alt,seed_info_detail,investor} from "../controllers/heimdall.controller.js";
 
 const router = Router();
 
@@ -24,4 +24,6 @@ router.route('/seed_information').get(seed_information);
 router.route('/investor_alt').get(investor_alt);
 
 router.route('/seed_info_detail').get(seed_info_detail)
+
+router.route('/investor').get(investor);
 export default router;
